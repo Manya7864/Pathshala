77425c91322efd520927976ff92bb9564a2b6e37
Manya7864 <manishyadav78642080@gmail.com>
2023-03-23 11:47:13 +0530

    completed

---
+++ Frontend/pathshala/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MainRoutes from './MainRoutes/MainRoutes';
+import Navbar from './Component/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <MainRoutes/>
+    </div>
+  );
+}
+
+export default App;
